fa89587b1c16d55dd09bd11139a5867784dc36a0
Nathan N <Nitharios@users.noreply.github.com>
2018-01-13 16:02:40 -1000

    Fixed title

--- src/routes/forms/index.js
+++ src/routes/forms/index.js
@@ -11,7 +11,7 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import Forms from './Forms';
 
-const title = 'Contact Us';
+const title = 'Forms';
 
 function action() {
   return {
