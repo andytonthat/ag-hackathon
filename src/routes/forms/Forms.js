59c4f73cacb5b53f5d36af1dd8a6999c6e303181
Nathan N <Nitharios@users.noreply.github.com>
2018-01-13 16:34:18 -1000

    Added missing titles

--- src/routes/forms/Forms.js
+++ src/routes/forms/Forms.js
@@ -8,15 +8,21 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Link from '../../components/Link';
 import s from './Forms.css';
 
 class Forms extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
+          <h1>{this.props.title}</h1>
           <p className={s.title}>General Requirements</p>
           <Link className={s.link} to="/traceback">
             Traceback Log
--- src/routes/resources/Resources.js
+++ src/routes/resources/Resources.js
@@ -8,14 +8,20 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Resources.css';
 
 class Resources extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
+          <h1>{this.props.title}</h1>
           <p>...</p>
         </div>
       </div>
